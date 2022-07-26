da9ed2c0fa7783da582d6a94df15968033b084dc
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-07-26 21:27:48 +0300

    Fixed server port variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const { dbConnect } = require("./config/database.js");
 const route = require("./routes.js");
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
